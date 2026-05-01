91d2e3fb4f099106024a198b16e562e75aea83a1
alvinwilta <13519163@std.stei.itb.ac.id>
2026-05-01 22:14:21 +0700

    fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,10 @@
 import { unstable_dev } from "wrangler";
-import type { UnstableDevWorker } from "wrangler";
+import type { Unstable_DevWorker } from "wrangler";
 import { describe, expect, it, beforeAll, afterAll } from "vitest";
 
 // TODO: Create tests
 describe("Worker", () => {
-  let worker: UnstableDevWorker;
+  let worker: Unstable_DevWorker;
 
   beforeAll(async () => {
     worker = await unstable_dev("src/index.ts", {
@@ -16,11 +16,10 @@ describe("Worker", () => {
     await worker.stop();
   });
 
-  it("should return Hello World", async () => {
+  it("should return 200 on home route", async () => {
     const resp = await worker.fetch();
-    if (resp) {
-      const text = await resp.text();
-      expect(text).toMatchInlineSnapshot(`"Hello World!"`);
-    }
+    expect(resp.status).toBe(200);
+    const text = await resp.text();
+    expect(text).toBe(JSON.stringify({ msg: "Server up and running" }));
   });
 });
